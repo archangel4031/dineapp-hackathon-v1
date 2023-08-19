40c0d77a7e15c0180f456f263abe5a52f1b14cc0
Malik Sahab <darkangel916@hotmail.com>
2023-08-19 07:12:23 +0500

    Minor changes, before Redux

--- src/app/products/[title]/page.tsx
+++ src/app/products/[title]/page.tsx
@@ -30,28 +30,38 @@ export default async function Page({ params }: Props) {
                     </p>
                     <p className="my-4 text-lg font-bold">Select Size</p>
                     <ul className="flex flex-row gap-8 justify-items-center font-bold text-gray-500">
-                        <li className="rounded-full hover:cursor-pointer hover:shadow-gray-500 hover:text-red-500 hover:shadow-xl p-2">
+                        <li className="rounded-full flex justify-center items-center hover:cursor-pointer hover:shadow-gray-500 hover:text-red-500 hover:shadow-xl p-2 w-12 h-12">
                             XS
                         </li>
-                        <li className="rounded-full hover:cursor-pointer hover:shadow-gray-500 hover:text-red-500 hover:shadow-xl p-2">
+                        <li className="rounded-full flex justify-center items-center hover:cursor-pointer hover:shadow-gray-500 hover:text-red-500 hover:shadow-xl p-2 w-12 h-12">
                             S
                         </li>
-                        <li className="rounded-full hover:cursor-pointer hover:shadow-gray-500 hover:text-red-500 hover:shadow-xl p-2">
+                        <li className="rounded-full flex justify-center items-center hover:cursor-pointer hover:shadow-gray-500 hover:text-red-500 hover:shadow-xl p-2 w-12 h-12">
                             M
                         </li>
-                        <li className="rounded-full hover:cursor-pointer hover:shadow-gray-500 hover:text-red-500 hover:shadow-xl p-2">
+                        <li className="rounded-full flex justify-center items-center hover:cursor-pointer hover:shadow-gray-500 hover:text-red-500 hover:shadow-xl p-2 w-12 h-12">
                             L
                         </li>
-                        <li className="rounded-full hover:cursor-pointer hover:shadow-gray-500 hover:text-red-500 hover:shadow-xl p-2">
+                        <li className="rounded-full flex justify-center items-center hover:cursor-pointer hover:shadow-gray-500 hover:text-red-500 hover:shadow-xl p-2 w-12 h-12">
                             XL
                         </li>
                     </ul>
-                    <p className="my-4">Quantity: 1</p>
                     <div className="flex gap-4 items-center my-4">
-                        <Button>
-                            <ShoppingCart /> ADD TO CART
+                        <p>Quantity:</p>
+
+                        <p className="border border-gray-800 rounded-full flex justify-center items-center w-9 h-9 cursor-pointer">
+                            -
+                        </p>
+                        {/* <p>{quantity}</p> */}
+                        <p className="border border-gray-800 rounded-full flex justify-center items-center w-9 h-9 cursor-pointer">
+                            +
+                        </p>
+                    </div>
+                    <div className="flex gap-4 items-center my-4">
+                        <Button className="px-4 py-6">
+                            <ShoppingCart className="mx-4" /> ADD TO CART
                         </Button>
-                        <p className="text-xl font-bold self-center">
+                        <p className="text-2xl font-bold self-center">
                             $ {product.price}
                         </p>
                     </div>
